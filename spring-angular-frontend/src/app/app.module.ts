d3b3dfa2426d8842aa129ac184dbbb97e2c5f4e2
Prem <Premtammina@gmail.com>
2019-10-21 00:26:20 -0400

    header and footer, bootstrap css file added

--- spring-angular-frontend/src/app/app.module.ts
+++ spring-angular-frontend/src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './com/pages/welcome/welcome.component';
 import { LoginComponent } from './com/pages/login/login.component';
 import { ErrorComponent } from './com/pages/error/error.component';
 import { ListTodosComponent } from './com/todos/list-todos/list-todos.component';
+import { HeaderComponent } from './com/pages/header/header.component';
+import { FooterComponent } from './com/pages/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +18,9 @@ import { ListTodosComponent } from './com/todos/list-todos/list-todos.component'
     WelcomeComponent,
     LoginComponent,
     ErrorComponent,
-    ListTodosComponent
+    ListTodosComponent,
+    HeaderComponent,
+    FooterComponent    
   ],
   imports: [
     BrowserModule,
